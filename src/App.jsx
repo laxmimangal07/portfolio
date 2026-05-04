eb444c459a7e8f7a5228257cfc994523b2a280fa
Apnacollege-demo <laxmimangal07@gmail.com>
2026-05-04 07:38:05 +0530

    fix Contact import case issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Contact from "./components/contact";
 
 function App() {
   return (
